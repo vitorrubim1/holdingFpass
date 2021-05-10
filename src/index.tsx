e172cc0d09edc8d894cda0d443811652cd70024b
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-05-10 11:44:28 -0300

    chore: configuring the chakra ui framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
+import { theme } from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
